081b434250f985ef8e18dedb746596af301020dd
Kant Lim <yju0808@naver.com>
2025-07-16 14:17:49 +0000

    feat: spring data jpa example

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -19,7 +19,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 // @Import(JdbcTemplateV2Config.class)
 // @Import(JdbcTemplateV3Config.class)
 // @Import(MyBatisConfig.class)
-@Import(JpaConfig.class)
+// @Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 	public static void main(String[] args) {
