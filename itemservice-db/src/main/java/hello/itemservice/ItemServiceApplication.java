7111473178892e1cebd2432f3eb62295b759e86e
Kant Lim <yju0808@naver.com>
2025-07-16 10:48:15 +0000

    feat: jpa example

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,7 +18,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 // @Import(JdbcTemplateV1Config.class)
 // @Import(JdbcTemplateV2Config.class)
 // @Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+// @Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 	public static void main(String[] args) {
--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,14 +1,20 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import jakarta.persistence.*;
 
 @Data
+@Entity
 public class Item {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+
     private Integer price;
+    
     private Integer quantity;
 
     public Item() {
