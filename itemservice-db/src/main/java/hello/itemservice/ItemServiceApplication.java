618dfb07663f415ccf46ad318e63f53910c067f2
Kant Lim <yju0808@naver.com>
2025-07-17 10:05:53 +0000

    feat: querydsl example

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -20,7 +20,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 // @Import(JdbcTemplateV3Config.class)
 // @Import(MyBatisConfig.class)
 // @Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+// @Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 	public static void main(String[] args) {
